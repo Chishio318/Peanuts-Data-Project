b3c8b78783e93dae9a258ba2d00971bb1adb5b78
Chishio318 <chishio.furukawa@gmail.com>
2021-11-07 19:04:22 +0900

    making new updates with document

--- 01_admin/preamble/admin.R
+++ 01_admin/preamble/admin.R
@@ -6,3 +6,10 @@ renv::restore()
 
 cat("\014")   ###clears console
 if(!is.null(dev.list())) dev.off()
+
+library("tidyverse")
+tinytex::install_tinytex()
+tinytex::tlmgr_install(c("collection-langgerman",
+                         "collection-langjapanese",
+                         "koma-script",
+                         "enumitem", "setspace", "xpatch"))
