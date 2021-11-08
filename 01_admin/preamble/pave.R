57eca9b7492fd5b6058cfae688bbae520c9ffdab
Chishio318 <chishio.furukawa@gmail.com>
2021-11-08 09:22:11 +0900

    file name changes

--- 01_admin/preamble/pave.R
+++
@@ -1,9 +0,0 @@
-# rm(list = ls())
-Sys.setenv(LANG = "en_US.UTF-8") #set error message in English
-
-cat("\014")   #clears console
-if(!is.null(dev.list())) dev.off()  #clear figures
-
-renv::restore() #update package lists
-
-library("tidyverse")
