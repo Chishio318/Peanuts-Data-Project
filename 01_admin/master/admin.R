b1cde681d46901cda2e006ebcf40e37bff161cc6
Chishio318 <chishio.furukawa@gmail.com>
2021-11-16 05:50:10 +0900

    rename to master from homerun

---
+++ 01_admin/master/admin.R
@@ -0,0 +1,75 @@
+main <- function(){
+  assemble()
+  build()
+  analyze()
+  #report()
+  settle()
+}
+
+
+assemble <- function(){
+  lets('assemble', 'preamble')
+  lets('assemble', 'function_libraries')
+}
+
+
+build <- function(){
+  lets('build', 'test_score_tidy')
+  lets('build', 'breakfast_tidy')
+  lets('build', 'test_score_ready')
+  lets('build', 'breakfast_ready')
+  lets('build', 'master')
+}
+
+
+analyze <- function(){
+  lets('analyze', 'test_score_tidy_check')
+  lets('analyze', 'breakfast_tidy_check')
+  lets('analyze', 'test_score_ready_check')
+  lets('analyze', 'breakfast_ready_check')
+  
+  lets('analyze', 'pancakes_study')
+  lets('analyze', 'dog_flakes_study')
+}
+
+
+report <- function(){
+  lets('report', 'checks')
+}
+
+
+settle <- function(){
+  lets('settle', 'postamble')
+}
+
+lets <- function(verb_name, object_name){
+  if (verb_name == 'assemble' && object_name == 'preamble'){
+    source(here::here('01_admin', object_name, 'R', 'admin.R'))
+  }
+  
+  else if (verb_name == 'assemble' && object_name == 'function_libraries'){
+    source(here::here('01_admin', object_name, 'admin.R'))
+  }
+  
+  else if (verb_name == 'build'){
+    source(here::here('03_build', object_name, 'code', 'build.R'))
+  }
+  
+  else if (verb_name == 'analyze'){
+    source(here::here('04_analyze', object_name, 'code', 'analyze.R'))
+  }
+  
+  else if (verb_name == 'report'){
+    rmarkdown::render(here::here('05_report', 
+                                 object_name, 'text', 'report.Rmd'),
+                      output_dir = here::here('05_report', 
+                                              object_name, 'output')) 
+  }
+  
+  else if (verb_name == 'settle' && object_name == 'postamble'){
+    source(here::here('01_admin', object_name, 'admin.R'))
+  }
+  
+}
+
+main()
