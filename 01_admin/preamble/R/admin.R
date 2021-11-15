79a202b5eab2e5b23ac07ad3a56bd6579123dc1b
Chishio318 <chishio.furukawa@gmail.com>
2021-11-16 05:50:43 +0900

    relocate preambles

--- 01_admin/preamble/R/admin.R
+++ 01_admin/preamble/R/admin.R
@@ -2,6 +2,7 @@ main <- function(){
   set_error_to_English()
   clear_environment()
   prepare_packages()
+  initialize_time()
 }
 
 
@@ -9,19 +10,21 @@ set_error_to_English <- function(){
   Sys.setenv(LANG = "en_US.UTF-8") 
 }
 
-
 clear_environment <- function(){
   cat("\014")   # console
-  if(!is.null(dev.list())) dev.off() #figures
+  if(!is.null(dev.list())) dev.off() # figures
+  gc();  gc(); # garbage collection
 }
-
 
 prepare_packages <- function(){
   renv::restore()
   devtools::update_packages(pkgs = TRUE)
-  library("tidyverse")
+  library("magrittr")
   tinytex::install_tinytex()
 }
 
+initialize_time <- function(){
+  tictoc::tic("master")
+}
 
 main()
