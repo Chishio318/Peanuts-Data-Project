05421af0c682885eb19cb195ca73adbae0ec6520
Chishio318 <chishio.furukawa@gmail.com>
2021-11-23 13:34:54 +0900

    update admin

--- 01_admin/03_function_libraries/admin.R
+++ 01_admin/03_function_libraries/admin.R
@@ -1,23 +1,70 @@
-main <- function(){
-  library_lists() -> lists
-  load_functions(lists)
+read_interim <- function(folder_name){
+  e <- new.env()
+  sys.source("01_admin/03_function_libraries/read_and_save.R", 
+             envir = e)
+  output <- do.call("read_interim", 
+                    list(folder_name), 
+                    envir = e)
+  
+  return(output)
 }
 
-library_lists <- function(){
-  lists <- c("read_and_save",
-             "elucidate_quantile")
-  #"elucidate_table.R"
-  return(lists)
+save_interim <- function(data, folder_name){
+  e <- new.env()
+  sys.source("01_admin/03_function_libraries/read_and_save.R", 
+             envir = e)
+  do.call("save_interim", 
+          list(data, folder_name), 
+          envir = e)
 }
 
-load_functions <- function(lists){
-  purrr::map(lists, load_each_function)
+save_my_plot <- function(my_plot, folder_name){
+  e <- new.env()
+  sys.source("01_admin/03_function_libraries/read_and_save.R", 
+             envir = e)
+  do.call("save_my_plot", 
+                    list(my_plot, folder_name), 
+                    envir = e)
+}
+      
+elucidate_quantile <- function(data, var_name, output_folder){
+  var_name <- rlang::enquo(var_name)
+  
+  e <- new.env()
+  sys.source("01_admin/03_function_libraries/elucidate_quantile.R", 
+             envir = e)
+  do.call("elucidate_quantile", 
+          list(data, var_name, output_folder), 
+          envir = e)
 }
 
-load_each_function <- function(lib_name){
-  file_name0 <- paste0(lib_name, ".R")
-  file_name <- here::here("01_admin", "03_function_libraries", file_name0)
-  source(file_name)
+elucidate_table <- function(data, var_name, output_folder){
+  var_name <- rlang::enquo(var_name)
+  
+  e <- new.env()
+  sys.source("01_admin/03_function_libraries/elucidate_table.R", 
+             envir = e)
+  do.call("elucidate_table", 
+          list(data, var_name, output_folder), 
+          envir = e)
 }
 
-main()
+check_quantitative <- function(data, var_vector, folder_name){
+  
+  e <- new.env()
+  sys.source("01_admin/03_function_libraries/elucidate_together.R", 
+             envir = e)
+  do.call("check_quantitative", 
+          list(data, var_vector, folder_name), 
+          envir = e)
+}
+
+check_categorical <- function(data, var_vector, folder_name){
+  
+  e <- new.env()
+  sys.source("01_admin/03_function_libraries/elucidate_together.R", 
+             envir = e)
+  do.call("check_categorical", 
+          list(data, var_vector, folder_name), 
+          envir = e)
+}
