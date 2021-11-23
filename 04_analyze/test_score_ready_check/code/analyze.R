d93d2f768b02eff64cd6ffaf1c10d9db8a9d3e70
Chishio318 <chishio.furukawa@gmail.com>
2021-11-23 10:28:26 +0900

    pancakes study figure

--- 04_analyze/test_score_ready_check/code/analyze.R
+++ 04_analyze/test_score_ready_check/code/analyze.R
@@ -1,6 +1,13 @@
 main <- function(){
+  my_folder <- "test_score_ready"
+  my_data <- read_interim(my_folder)
+  
+  var_quantitative <- c("implied_test")
+    
+  check_quantitative(my_data, 
+                    var_quantitative, 
+                    my_folder)
 }
-
 
 
 main()
